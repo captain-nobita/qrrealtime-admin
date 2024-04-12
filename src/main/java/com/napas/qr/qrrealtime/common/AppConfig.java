0bd2bcc6539c76698cf17b6927f117458c99c707
huynx <huynx@napas.com.vn>
2024-04-12 15:08:52 +0700

    fix

--- src/main/java/com/napas/qr/qrrealtime/common/AppConfig.java
+++ src/main/java/com/napas/qr/qrrealtime/common/AppConfig.java
@@ -6,15 +6,35 @@
 package com.napas.qr.qrrealtime.common;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author huynx
  */
-@Configuration
-@Data
+@ConfigurationProperties(prefix = "app")
+@Component
 public class AppConfig {
     private String napasKeyId = "napas";
     private String merchantAgentId = "971133";
+
+    public String getNapasKeyId() {
+        return napasKeyId;
+    }
+
+    public void setNapasKeyId(String napasKeyId) {
+        this.napasKeyId = napasKeyId;
+    }
+
+    public String getMerchantAgentId() {
+        return merchantAgentId;
+    }
+
+    public void setMerchantAgentId(String merchantAgentId) {
+        this.merchantAgentId = merchantAgentId;
+    }
+    
+    
 }
